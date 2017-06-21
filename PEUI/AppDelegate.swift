69d3e4e5bab365809063af338193c794b59fee79
Alexander Snegursky <a.snegurskiy@mobidev.biz>
2017-06-21 20:39:08 +0200

    Create project

---
+++ PEUI/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PEUI
+//
+//  Created by Alexander Snegursky on 6/21/17.
+//  Copyright © 2017 Alexander Snegursky. All rights reserved.
+//
+
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ PEUI/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PEUI
+//
+//  Created by Alexander Snegursky on 6/21/17.
+//  Copyright © 2017 Alexander Snegursky. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+}
+
